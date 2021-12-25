cdcbdf6de89ded3038167d3c69439195d91a0f25
Shinde <sachin.g.shinde@capgemini.com>
2021-12-25 10:08:19 -0500

    Vertical Order Traversal of a Binary Tree

---
+++ src/in/sachinshinde/binarytree/Tuple.java
@@ -0,0 +1,13 @@
+package in.sachinshinde.binarytree;
+
+class Tuple {
+	Node node;
+	int row;
+	int col;
+	
+	public Tuple(Node node, int row, int col){
+		this.node = node;
+		this.row = row;
+		this.col = col;
+	}
+}
