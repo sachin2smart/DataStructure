63c766b62752e66df921d5da5bf52c4af08bad7b
Shinde <sachin.g.shinde@capgemini.com>
2021-12-26 07:07:05 -0500

    Level order Traversal of a Binary Tree

--- src/in/sachinshinde/binarytree/LevelOrderTreeTraversal.java
+++ src/in/sachinshinde/binarytree/LevelOrderTreeTraversal.java
@@ -1,6 +1,8 @@
 package in.sachinshinde.binarytree;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class LevelOrderTreeTraversal {
@@ -13,15 +15,43 @@ public class LevelOrderTreeTraversal {
 		while(!queue.isEmpty()) {
 	
 			Node currNode = queue.poll();
-			System.out.print(currNode.key);
+			System.out.print(" "+currNode.key);
 			
 			if(currNode.left!=null ) 
 				queue.add(currNode.left);
 					
 			if(currNode.right!=null)
 				queue.add(currNode.right);
-				
 		}
+	}
+	
+	private static List<List<Integer>> getLevelOrderOfBinaryTree(Node root){
+		List<List<Integer>> wrapList = new LinkedList<>();
+		
+		if(root == null) 
+			return wrapList;
+		
+		Queue<Node> q = new LinkedList<>();
+		q.offer(root);
+		
+		while(!q.isEmpty()) {
+			List<Integer> subList = new LinkedList<Integer>();
+			int n = q.size();
+			
+			for(int i=0; i<n; i++) {
+				if(q.peek().left != null)
+					q.offer(q.peek().left);
+				
+				if(q.peek().right != null)
+					q.offer(q.peek().right);
+				
+				subList.add(q.poll().key);
+			}
+			
+			wrapList.add(subList);
+		}
+		
+		return wrapList;
 	}
 	
 	public static void main(String[] args) {
@@ -35,6 +65,13 @@ public class LevelOrderTreeTraversal {
 		
 		printNodeLevelwise(root);
 		
+		System.out.print("\n\n Level Order traversal : ");
+		List<List<Integer>> result = getLevelOrderOfBinaryTree(root);
+	
+		result.forEach((list) -> {
+			System.out.println();
+            list.forEach((num) -> System.out.print(" "+num));
+        });
 	}
 
 }
