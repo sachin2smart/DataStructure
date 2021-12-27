63e4ad4cd6b68f806e7c13c69f3ecb2d10b5f59a
Shinde <sachin.g.shinde@capgemini.com>
2021-12-26 20:39:09 -0500

    Get diameter of a Binary Tree

---
+++ src/in/sachinshinde/binarytree/DiameterOfBinaryTree.java
@@ -0,0 +1,53 @@
+package in.sachinshinde.binarytree;
+
+public class DiameterOfBinaryTree {
+
+	private static int max;
+	
+	private static int diameterOfBinaryTree(Node root) {
+		
+		if(root == null)
+			return 0;
+		
+		int height = 0;
+		
+		int l_height = diameterOfBinaryTree(root.left);
+		int r_height = diameterOfBinaryTree(root.right);
+		
+		height = Math.max(l_height, r_height);
+		height++;
+		
+		max = Math.max(max, l_height + r_height + 1);
+		return height;
+	}
+	
+	private static int getDiameterOfBinaryTree(Node root) {
+		max = Integer.MIN_VALUE;
+		diameterOfBinaryTree(root);
+		return max-1;
+	}
+	
+	public static void main(String[] args) {
+		Node root = new Node(1);
+		root.left = new Node(2);
+		root.left.left = new Node(3);
+		root.left.left.left = new Node(4);
+		root.left.left.right = new Node(5);
+		root.left.left.right.right = new Node(6);
+		root.left.left.right.right.right = new Node(7);
+		root.left.right = new Node(8);
+		root.left.right.right = new Node(9);
+		root.left.right.right.right = new Node(10);
+		root.left.right.right.right.right = new Node(11);
+		root.right = new Node(12);
+		
+		System.out.println("The diameter of a binary tree is : " + getDiameterOfBinaryTree(root)); //8
+		
+		Node n = new Node(1);
+		n.left = new Node(2);
+		n.right = new Node(3);
+		
+		System.out.println("\nThe diameter of a binary tree is : " + getDiameterOfBinaryTree(n)); //2
+		
+	}
+}
--- src/in/sachinshinde/binarytree/LevelOrderTreeTraversalInSpiralForm.java
+++ src/in/sachinshinde/binarytree/LevelOrderTreeTraversalInSpiralForm.java
@@ -50,7 +50,7 @@ public class LevelOrderTreeTraversalInSpiralForm {
 		root.left = new Node(8);
 		root.left.left = new Node(3);
 		root.left.right = new Node(5);
-		root.right = new Node(2);
+		root.right = new Node(7);
 		root.right.left = new Node(2);
 		
 		printLevelwiseSpriralTree(root);
