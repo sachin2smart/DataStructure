f477ca5b93c3ba96f8c4f8c1512768b50b892b8f
Sachin Shinde <sachin2smart@gmail.com>
2021-12-30 04:26:15 -0500

    Selection Sort Algorithm - structured

--- src/in/sachinshinde/sort/SelectionSort.java
+++ src/in/sachinshinde/sort/SelectionSort.java
@@ -18,17 +18,16 @@ public class SelectionSort {
         int n = arr.length;
  
         for (int i = 0; i < n-1; i++) {
-        // Find the minimum element in unsorted array
             int min_idx = i;
             for (int j = i+1; j < n; j++)
                 if (arr[j] < arr[min_idx])
                     min_idx = j;
  
-        // Swap this minimum index element with the i'th element
-        int temp = arr[min_idx];
-        arr[min_idx] = arr[i];
-        arr[i] = temp;
-    }
+	        // Swap this minimum index element with the i'th element
+	        int temp = arr[min_idx];
+	        arr[min_idx] = arr[i];
+	        arr[i] = temp;
+        }
   }
 	
 }
