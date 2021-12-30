7ea346ef6ea28ae70c1931f99ab03ade61d0dfb7
Sachin Shinde <sachin2smart@gmail.com>
2021-12-30 05:21:06 -0500

    Topologica Sort Algorithm - An exercise

---
+++ src/in/sachinshinde/sort/TopologicalSort.java
@@ -0,0 +1,19 @@
+package in.sachinshinde.sort;
+
+// Graph should be DAG : Directed Acyclic Graph
+
+/*
+	1. Adjacency List - BFS
+	2. Adjacency List - DFS
+	3. Adjacency Matrix - BFS
+	4. Adjacency Matrix - DFS
+	
+	BFS  (Queue, No Recursion)
+	DFS	 (Stack, Recursion)
+*/
+
+public class TopologicalSort {
+	
+	// DFS version with Adjacency List is already being exercised in ../graph/TopoSortByDFS.java
+	
+}
