55ade158e13628e6adb21b6e90d1b8bfa91b6115
Sachin Shinde <sachin2smart@gmail.com>
2021-12-30 23:13:37 -0500

    Merge Two Sorted Lists

---
+++ src/in/sachinshinde/linkedlist/MergeTwoSortedLists.java
@@ -0,0 +1,23 @@
+package in.sachinshinde.linkedlist;
+
+//	https://leetcode.com/problems/merge-two-sorted-lists/
+
+public class MergeTwoSortedLists {
+
+	public LinkedListNode mergeTwoLists(LinkedListNode l1, LinkedListNode l2) {
+		if(l1 == null)
+			return l2;
+		
+		if(l2 == null)
+			return l1;
+		
+		if(l1.data < l2.data) {
+			l1.next = mergeTwoLists(l1.next, l2);
+			return l1;
+		}
+		else {
+			l2.next = mergeTwoLists(l2.next, l1);
+			return l2;
+		}
+	}
+}
