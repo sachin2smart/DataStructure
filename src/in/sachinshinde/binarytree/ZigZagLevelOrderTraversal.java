397d6f4a5948b40471957030d87337a37f1c3932
Sachin Shinde <sachin2smart@gmail.com>
2022-01-03 14:13:00 -0500

    ZigZag Level Order Traversal

---
+++ src/in/sachinshinde/binarytree/ZigZagLevelOrderTraversal.java
@@ -0,0 +1,44 @@
+package in.sachinshinde.binarytree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+//	https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+
+/*
+ *	Given the root of a binary tree, return the zigzag level order traversal of its nodes' values. 
+ *		(i.e., from left to right, then right to left for the next level and alternate between). 
+ */
+
+public class ZigZagLevelOrderTraversal {
+	
+	public List<List<Integer>> zigzagLevelOrder(Node root) {
+		List<List<Integer>> ans = new ArrayList<>();
+		travel(root, ans, 0);
+		return ans;
+    }
+
+	private void travel(Node curr, List<List<Integer>> ans, int level) {
+		if(curr == null)
+			return;
+		
+		if(ans.size() <= level)
+        {
+            List<Integer> nextLevel = new LinkedList<>();
+            ans.add(nextLevel);
+        }
+        
+        List<Integer> currentLevelList  = ans.get(level);
+        
+        if(level % 2 == 0) 
+        	currentLevelList.add(curr.key);	//	this method will append the value to next node
+        else 
+        	currentLevelList.add(0, curr.key);	// The add(index, val) method inserts an element at a specified index in the list. 
+        										//	It shifts the element currently at that position (if any) and 
+        										//	any subsequent elements to the right (will add one to their indices).
+        
+		travel(curr.left, ans, level+1);
+		travel(curr.right, ans, level+1);
+	}
+}
