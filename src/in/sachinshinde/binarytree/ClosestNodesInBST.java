96cdc21418ba59dc5c850f14823c25907aee6863
Sachin Shinde <sachin2smart@gmail.com>
2022-01-09 16:04:43 -0500

    Closest Nodes In BST -Leetcode statement added

--- src/in/sachinshinde/binarytree/ClosestNodesInBST.java
+++ src/in/sachinshinde/binarytree/ClosestNodesInBST.java
@@ -5,6 +5,35 @@ import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 
+//	https://leetcode.ca/2016-08-28-272-Closest-Binary-Search-Tree-Value-II/
+
+/*
+ * 	 Given a non-empty binary search tree and a target value,
+ 	 Find k values in the BST that are closest to the target.
+
+ Example:
+
+ Input: root = [4,2,5,1,3], target = 3.714286, and k = 2
+
+    4
+   / \
+  2   5
+ / \
+1   3
+
+ Output: [4,3]
+
+ Note:
+
+ Given target value is a floating point.
+ You may assume k is always valid, that is: k ≤ total nodes.
+ You are guaranteed to have only one unique set of k values in the BST that are closest to the target.
+
+ Follow up:
+ Assume that the BST is balanced, could you solve it in less than O(n) runtime (where n = total nodes)?
+
+ */
+
 public class ClosestNodesInBST {
 	
 	private static Deque<Integer> result = new ArrayDeque<Integer>();
