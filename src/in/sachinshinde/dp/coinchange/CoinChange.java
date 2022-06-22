0956c190d20a6dba4c99a940f966bb86140ed7e1
Sachin Shinde <sachin2smart@gmail.com>
2022-06-22 07:40:57 -0400

    Coin Change - DP - Leetcode

--- src/in/sachinshinde/dp/coinchange/CoinChange.java
+++ src/in/sachinshinde/dp/coinchange/CoinChange.java
@@ -38,6 +38,20 @@ public class CoinChange {
         return change[amount];
     }
 	
+	public int coinChange(int[] coins, int amount) {
+	    int[] dp = new int[amount + 1];
+	    
+	    for (int i=1; i < dp.length; i++) {
+	      dp[i] = dp.length;
+	      for (int j=0; j < coins.length; j++)
+	        if (i >= coins[j]) 
+	        	dp[i] = Math.min(dp[i], dp[i-coins[j]] + 1);
+	      
+	    }
+	    
+	    return dp[amount] == dp.length ? -1 : dp[amount];
+	 }
+	
 	public static void main(String[] args) {
 		System.out.println(getMinCoinChangeNeeded(new int[]{5,5,1}, 13));	//5 	5+5+1+1+1
 	}
