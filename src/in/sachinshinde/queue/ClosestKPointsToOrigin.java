96aa70ccdd6b4e8e467762d27dfdfbfd8903f18e
Sachin Shinde <sachin2smart@gmail.com>
2022-06-29 18:06:23 -0400

    ClosestKPointsToOrigin with different approaches - Queue(PrioriryQueue) MinHeap - Leetcode

--- src/in/sachinshinde/queue/ClosestKPointsToOrigin.java
+++ src/in/sachinshinde/queue/ClosestKPointsToOrigin.java
@@ -34,7 +34,56 @@ public class ClosestKPointsToOrigin {
     
     public static void main(String[] args) {
 		ClosestKPointsToOrigin closestKPointsToOrigin = new ClosestKPointsToOrigin();
+		//	MinHeap Solution, PriorityQueue : O(NlogK)
 		System.out.println(Arrays.deepToString(closestKPointsToOrigin.kClosest(new int[][] {{1,3},{-2,2}}, 1)));	//	[[-2, 2]]
 		System.out.println(Arrays.deepToString(closestKPointsToOrigin.kClosest(new int[][] {{3,3},{5,-1},{-2,4}}, 2)));	//	[[3, 3], [-2, 4]]
+		
+		//	Arrays.sort, Arrays.copyOfRange : O(NlogN)
+		System.out.println(Arrays.deepToString(closestKPointsToOrigin.kClosest2(new int[][] {{1,3},{-2,2}}, 1)));	//	[[-2, 2]]
+		System.out.println(Arrays.deepToString(closestKPointsToOrigin.kClosest2(new int[][] {{3,3},{5,-1},{-2,4}}, 2)));	//	[[3, 3], [-2, 4]]
+		
+		//	QuickSort : O(N)
+		System.out.println(Arrays.deepToString(closestKPointsToOrigin.kClosest3(new int[][] {{1,3},{-2,2}}, 1)));	//	[[-2, 2]]
+		System.out.println(Arrays.deepToString(closestKPointsToOrigin.kClosest3(new int[][] {{3,3},{5,-1},{-2,4}}, 2)));	//	[[3, 3], [-2, 4]]
 	}
+    
+    public int[][] kClosest2(int[][] points, int K) {
+        Arrays.sort(points, (p1, p2) -> p1[0] * p1[0] + p1[1] * p1[1] - p2[0] * p2[0] - p2[1] * p2[1]);
+        return Arrays.copyOfRange(points, 0, K);
+    }
+    
+    public int[][] kClosest3(int[][] points, int K) {
+        int len =  points.length, l = 0, r = len - 1;
+        while(l <= r) {
+            int mid = pivotPos(points, l, r);
+            if(mid == K) 
+            	break;
+            if(mid < K)
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+        return Arrays.copyOfRange(points, 0, K);
+    }
+
+    private int pivotPos(int[][] A, int l, int r) {
+        int[] pivot = A[l];
+        while(l < r) {
+            while(l < r && compare(A[r], pivot) >= 0) 
+            	r--;
+            
+            A[l] = A[r];
+            
+            while(l < r && compare(A[l], pivot) <= 0)
+            	l++;
+            
+            A[r] = A[l];
+        }
+        A[l] = pivot;
+        return l;
+    }
+
+    private int compare(int[] p1, int[] p2) {
+        return p1[0] * p1[0] + p1[1] * p1[1] - p2[0] * p2[0] - p2[1] * p2[1];
+    }
 }
