e97be0a4e0d567c0672a1f76128491d3f3ee133e
Sachin Shinde <sachin2smart@gmail.com>
2022-09-13 06:54:01 -0400

    Average at level in Binary Tree - BinaryTree - Leetcode

--- src/in/sachinshinde/binarytree/AverageAtLevelInBinaryTree.java
+++ src/in/sachinshinde/binarytree/AverageAtLevelInBinaryTree.java
@@ -8,7 +8,8 @@ import java.util.Queue;
 //	https://leetcode.com/problems/average-of-levels-in-binary-tree/
 
 /*
- * 	Given the root of a binary tree, return the average value of the nodes on each level in the form of an array.
+ * 	Given the root of a binary tree, 
+ * 	return the average value of the nodes on each level in the form of an array.
  */
 
 public class AverageAtLevelInBinaryTree {
@@ -22,21 +23,30 @@ public class AverageAtLevelInBinaryTree {
 	    q.add(root);
 	    
 	    while(!q.isEmpty()) {
-	        int n = q.size();
-	        double sum = 0.0;
+	        int n = q.size();	// this is divisor
+	        double sum = 0.0;	// this is dividend
 	        
 	        for(int i = 0; i < n; i++) {
 	            Node node = q.poll();
 	            sum += node.key;
-	            
+	            // prepare a queue for next level
 	            if(node.left != null) 
 	            	q.offer(node.left);
 	            if(node.right != null) 
 	            	q.offer(node.right);
 	        }
 	        
-	        result.add(sum / n);
+	        result.add(sum / n);	// store the division result at each level
 	    }
 	    return result;
+	}
+	
+	public static void main(String[] args) {
+		Node root = new Node(1);
+		root.left = new Node(2);
+		root.right = new Node(3);
+		
+		AverageAtLevelInBinaryTree avg = new AverageAtLevelInBinaryTree();
+		System.out.println(avg.averageOfLevels(root));
 	}
 }
