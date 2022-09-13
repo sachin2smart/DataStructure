87e107385722abf7b74883d1ae390c98b0d146d4
Sachin Shinde <sachin2smart@gmail.com>
2022-09-13 07:08:24 -0400

    Balanced Binary Tree - BinaryTree - Leetcode

--- src/in/sachinshinde/binarytree/BalancedBinaryTree.java
+++ src/in/sachinshinde/binarytree/BalancedBinaryTree.java
@@ -5,23 +5,23 @@ package in.sachinshinde.binarytree;
 /*
  * 	Given a binary tree, determine if it is height-balanced.
 	## A height-balanced binary tree is defined as:
-			a binary tree in which the left and right subtrees of every node 
-			differ in height by no more than 1.
+			a binary tree in which the left and right subtrees 
+			of every node differ in height by no more than 1.
  */
 
 public class BalancedBinaryTree {
 	
 	public boolean isBalanced(Node root) {
-      if(root == null){
+      if(root == null) {
     	 return true;
 	  }
       
 	  return helper(root) != -1;
     }
     
-    private int helper(Node root){
+    private int helper(Node root) {
     	//	Base Condition
-        if(root == null){
+        if(root == null) {
             return 0;
         }
         
@@ -31,7 +31,8 @@ public class BalancedBinaryTree {
         /*
         	## If leftNode of currentNode is null, return -1
         	## If rightNode of currentNode is null, return -1
-        	## If the difference between height of left and right node is greater than 1, return -1
+        	## If the difference between height of left and right node 
+        			is greater than 1, return -1
         */
    
         if(left == -1 || right == -1 || Math.abs(left - right) > 1){
@@ -41,4 +42,20 @@ public class BalancedBinaryTree {
         // Always return the maximum height of the current node
         return Math.max(left, right) + 1;
     }
+    
+    public static void main(String[] args) {
+		BalancedBinaryTree tree = new BalancedBinaryTree();
+		
+		Node n = new Node(1);
+		n.left = new Node(2);
+		n.right = new Node(3);
+		
+		System.out.println(tree.isBalanced(n));	//	true
+		
+		n.left.left = new Node(4);
+		System.out.println(tree.isBalanced(n));	// true
+		
+		n.left.left.left = new Node(5);
+		System.out.println(tree.isBalanced(n));	// false
+	}
 }
