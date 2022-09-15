751834db5d1fba7292fd5653c490bafe5cfd9952
Sachin Shinde <sachin2smart@gmail.com>
2022-09-15 08:19:03 -0400

    Invert Binary Tree - Binary Tree - Leetcode

--- src/in/sachinshinde/binarytree/InvertBinaryTree.java
+++ src/in/sachinshinde/binarytree/InvertBinaryTree.java
@@ -2,7 +2,8 @@ package in.sachinshinde.binarytree;
 
 //	https://leetcode.com/problems/invert-binary-tree/
 
-//	Given the root of a binary tree, invert the tree, and return its root.
+//	Given the root of a binary tree, invert the tree, 
+//	and return its root.
 //	Input: root = [4,2,7,1,3,6,9]
 //	Output:root = [4,7,2,9,6,3,1]
 
@@ -18,4 +19,42 @@ public class InvertBinaryTree {
         
         return root;
     }
+	
+	public Node invert(Node node) { 
+		if (node == null) 
+			return node; 
+		
+		Node left = invert(node.left); 
+		Node right = invert(node.right); 
+
+		node.left = right; 
+		node.right = left; 
+
+		return node; 
+	}
+	
+	public static void main(String[] args) {
+		InvertBinaryTree binaryTree = new InvertBinaryTree();
+		Node n = new Node(2);
+		n.left = new Node(1);
+		n.right = new Node(4);
+		n.right.left = new Node(3);
+		n.right.right = new Node(5);
+		
+		binaryTree.inOrder(n);	//	1 2 3 4 5
+		
+		Node root = binaryTree.invert(n);
+		
+		System.out.println();
+		binaryTree.inOrder(root);	// 5 4 3 2 1
+	}
+	
+	private void inOrder(Node root) {
+		if(root == null)
+			return;
+		
+		inOrder(root.left);
+		System.out.print(" " + root.key);
+		inOrder(root.right);
+	}
 }
