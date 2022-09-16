d7670a1ce89e9a01362d2b068ab00463991df6b2
Sachin Shinde <sachin2smart@gmail.com>
2022-09-15 21:45:34 -0400

    Min Depth of Tree - BinaryTree - Leetcode

--- src/in/sachinshinde/binarytree/MinDepthOfTree.java
+++ src/in/sachinshinde/binarytree/MinDepthOfTree.java
@@ -42,6 +42,43 @@ public class MinDepthOfTree {
 		root.right.right = new Node(8);	// depth = 3
 		root.right.right.right = new Node(9);	// depth = 4
 		
-		System.out.println("The max depth is : "+ getMinDepth(root));	//	4
+		System.out.println("The min depth is : "+ getMinDepth(root));	//	4
+		
+		MinDepthOfTree tree = new MinDepthOfTree();
+		System.out.println("The min depth is : "+ tree.minDepth2(root));	//	4
+		System.out.println("The min depth is : "+ tree.minDepth3(root));	//	4
+		System.out.println("The min depth is : "+ tree.minDepth4(root));	//	4
+	}
+	
+	public int minDepth2(Node root) {
+	    if (root == null) 
+	    	return 0;
+	    
+	    int L = minDepth2(root.left), 
+	    	R = minDepth2(root.right);
+	    
+	    return 1 + (Math.min(L, R) > 0 ? 
+	    				Math.min(L, R) : Math.max(L, R));
+	}
+
+	public int minDepth3(Node root) {
+	    if (root == null) 
+	    	return 0;
+	    
+	    int L = minDepth3(root.left), 
+	    	R = minDepth3(root.right), 
+	    	m = Math.min(L, R);
+	    
+	    return 1 + (m > 0 ? m : Math.max(L, R));
+	}
+
+	public int minDepth4(Node root) {
+	    if (root == null) 
+	    	return 0;
+	    
+	    int L = minDepth4(root.left), 
+	    	R = minDepth4(root.right);
+	    
+	    return L<R && L>0 || R<1 ? 1+L : 1+R;
 	}
 }
