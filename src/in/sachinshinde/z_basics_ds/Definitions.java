e75b204b2b238bfc41193b8050bb8815ba0a1028
Sachin Shinde <sachin2smart@gmail.com>
2022-09-16 12:13:15 -0400

    Definitions of few terms used in DS

---
+++ src/in/sachinshinde/z_basics_ds/Definitions.java
@@ -0,0 +1,35 @@
+package in.sachinshinde.z_basics_ds;
+
+public class Definitions {
+
+	/*
+	 	----------------------
+	 	1. Full binary tree 
+	 		It is a binary tree where each node has exactly 0 or 2 children.
+	  	----------------------
+	  	2. Height-balanced binary tree
+			a binary tree in which the left and right subtrees 
+			of every node differ in height by no more than 1
+		----------------------	
+		3. Diameter of a binary tree 
+			It is the length of the longest path between any two nodes in a tree.
+			The length of a path between two nodes is represented by 
+				the number of edges between them.
+ 			This path may or may not pass through the root.
+ 		----------------------
+ 		4. An inversion, or mirror, of a Binary Tree (T):
+ 			It is just a Binary Tree M(T) whose left and right children 
+ 			(of all non-leaf nodes) are swapped.
+ 		----------------------
+ 		5. Size of a tree:
+ 			It is the total number of elements present in the tree
+ 		----------------------
+ 		6. Maximum width of a tree 
+ 			It is the maximum width among all levels.
+ 			The width of one level is defined as the length between the end-nodes 
+			(the leftmost and rightmost non-null nodes), where 
+			the null nodes between the end-nodes that would be present in a complete binary tree 
+			extending down to that level are also counted into the length calculation.
+		----------------------
+	 */
+}
