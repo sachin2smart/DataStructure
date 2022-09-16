cd5db7af8788d05d303e7a0178abdab12b35d868
Sachin Shinde <sachin2smart@gmail.com>
2022-09-15 20:42:17 -0400

    Lowest Common Ancestor Of Binary Tree For Deepest Leaves - BinaryTree - Leetcode

--- src/in/sachinshinde/binarytree/LowestCommonAncestorOfBinaryTreeForDeepestLeaves.java
+++ src/in/sachinshinde/binarytree/LowestCommonAncestorOfBinaryTreeForDeepestLeaves.java
@@ -1,5 +1,7 @@
 package in.sachinshinde.binarytree;
 
+import java.util.HashMap;
+
 //	https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/
 
 /*
@@ -42,6 +44,97 @@ public class LowestCommonAncestorOfBinaryTreeForDeepestLeaves {
         }
         
         return Math.max(leftTreeDepth, rightTreeDepth); // return the maximum depth traversed so far
+    }
+    
+    //	2. Recursive approach : O(N^2)
+    public int height(Node root){
+        if(root == null)
+        	return 0;
+        
+        return Math.max(
+        		height(root.left), height(root.right)) + 1;
+    }
+    
+    public Node lcaDeepestLeaves2(Node root) {
+        if(root == null || 
+        		height(root.right) == height(root.left))
+        	return root;
+        
+        return lcaDeepestLeaves(
+        		height(root.left) > height(root.right) ?
+        				root.left	:	root.right);
+    }
+    
+    
+    //	3. Memoization : O(N)
+    HashMap<Node, Integer> heights = new HashMap<Node, Integer>();
+    public int heightMemorized(Node root){
+        if(root == null)
+        	return 0;
+        
+        if(heights.containsKey(root))
+        	return heights.get(root);
+        
+        heights.put(root , 1 + Math.max(heightMemorized(root.left),
+        								heightMemorized(root.right)));
+        
+        return heights.get(root);
+    }
+    
+    public Node lcaDeepestLeaves3(Node root) {
+        if(root == null || 
+        		heightMemorized(root.right) == heightMemorized(root.left))
+        	return root;
+        
+        return lcaDeepestLeaves3(
+        		heightMemorized(root.left) > heightMemorized(root.right) ?
+        				root.left	:	root.right);
+    }
+    
+    public static void main(String args[]) {
+		/*
+		        	1
+				 /      \
+			    2        3
+			  /   \     / \
+			 4     5   6   7
+			/ \   / \
+			8   9 10  11
+		      /     \
+		     12     13
+		     
+		     Answer :  8 4 12 9 2 10 13 5 11
+	    	
+		 */
+		
+		Node root = new Node(1);
+		root.left = new Node(2);
+		root.right = new Node(3);
+		root.left.left = new Node(4);
+		root.left.right = new Node(5);
+		root.right.left = new Node(6);
+		root.right.right = new Node(7);
+		root.left.left.left = new Node(8);
+		root.left.left.right = new Node(9);
+		root.left.right.left = new Node(10);
+		root.left.right.right = new Node(11);
+		root.left.left.right.left = new Node(12);
+		root.left.right.left.right = new Node(13);
+		
+		LowestCommonAncestorOfBinaryTreeForDeepestLeaves tree = 
+				new LowestCommonAncestorOfBinaryTreeForDeepestLeaves();
+		
+		Node resultNode = tree.lcaDeepestLeaves3(root);
+		tree.inOrder(resultNode);	//  8 4 12 9 2 10 13 5 11
+    }
+    
+    private void inOrder(Node root) {
+    	if(root == null)
+    		return;
+    	
+    	inOrder(root.left);
+    	System.out.print(" "+ root.key);
+    	inOrder(root.right);
     }
     
 }
