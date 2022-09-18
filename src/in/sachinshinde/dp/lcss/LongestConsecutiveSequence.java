5210b079cfca308a6a2df58c0d75a526d4c5bc56
Sachin Shinde <sachin2smart@gmail.com>
2022-09-18 12:09:12 -0400

    Longest Consecutive Sequence - DP - Leetcode

--- src/in/sachinshinde/dp/lcss/LongestConsecutiveSequence.java
+++ src/in/sachinshinde/dp/lcss/LongestConsecutiveSequence.java
@@ -1,5 +1,6 @@
 package in.sachinshinde.dp.lcss;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -68,5 +69,39 @@ public class LongestConsecutiveSequence {
 		
 		System.out.println(seq.longestConsecutive(
 				new int[] {0,3,7,2,5,8,4,6,0,1}));	//	9
+		
+		System.out.println(seq.longestConsecutive2(
+				new int[] {100,4,200,1,3,2}));	//	4
+		
+		System.out.println(seq.longestConsecutive2(
+				new int[] {0,3,7,2,5,8,4,6,0,1}));	//	9
+	}
+	
+	//	Using Memoization
+	private int longestConsecutive2(int[] nums) {
+		int res = 0;
+	    HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+	    for (int n : nums) {
+	        if (!map.containsKey(n)) {
+	            int left = (map.containsKey(n - 1)) ? map.get(n - 1) : 0;
+	            int right = (map.containsKey(n + 1)) ? map.get(n + 1) : 0;
+	            // sum: length of the sequence n is in
+	            int sum = left + right + 1;
+	            map.put(n, sum);
+	            
+	            // keep track of the max length 
+	            res = Math.max(res, sum);
+	            
+	            // extend the length to the boundary(s)
+	            // of the sequence
+	            // will do nothing if n has no neighbors
+	            map.put(n - left, sum);
+	            map.put(n + right, sum);
+	        }
+	        else {
+	            continue;	// duplicates
+	        }
+	    }
+	    return res;
 	}
 }
