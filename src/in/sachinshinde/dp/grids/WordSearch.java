ebf8cf7f5935ebc68c11c2a6da9525f4a9af5b31
Sachin Shinde <sachin2smart@gmail.com>
2022-09-19 09:49:23 -0400

    Word Search - DP,DFS,GRID - Leetcode

--- src/in/sachinshinde/dp/grids/WordSearch.java
+++ src/in/sachinshinde/dp/grids/WordSearch.java
@@ -10,42 +10,52 @@ package in.sachinshinde.dp.grids;
  */
 
 public class WordSearch {
-
-	static boolean[][] visited;
     
 	public boolean exist(char[][] board, String word) {
-        visited = new boolean[board.length][board[0].length];
+		boolean[][] visited = new boolean[board.length][board[0].length];
         
         for(int i = 0; i < board.length; i++)
             for(int j = 0; j < board[i].length; j++)
-                if((word.charAt(0) == board[i][j]) && isWordFound(board, word, i, j, 0))
+                if((word.charAt(0) == board[i][j]) && 
+                		isWordFound(board, i, j, word, 0, visited))
                     return true;
         
         return false;
     }
     
-    private boolean isWordFound(char[][]board, String word, int i, int j, int index){
-        if(index == word.length())
+    private boolean isWordFound(char[][]board, int i, int j, 
+    							String word, int wordIndex, 
+    							boolean[][] visited) {
+        if(wordIndex == word.length())
             return true;
         
-        
-        if(i >= board.length || i < 0 || j >= board[i].length || j < 0 || board[i][j] != word.charAt(index) || visited[i][j])
+        if(i >= board.length || i < 0 || j >= board[i].length || j < 0 || 
+        		board[i][j] != word.charAt(wordIndex) || visited[i][j])
             return false;
         
         visited[i][j] = true;
         
-        if(isWordFound(board, word, i-1, j, index+1) || 
-        		isWordFound(board, word, i+1, j, index+1) ||
-        		isWordFound(board, word, i, j-1, index+1) || 
-        		isWordFound(board, word, i, j+1, index+1))
-            return true;
-        
+        if(isWordFound(board, i-1, j, word, wordIndex+1, visited) || 
+           isWordFound(board, i+1, j, word, wordIndex+1, visited) ||
+           isWordFound(board, i, j-1, word, wordIndex+1, visited) || 
+           isWordFound(board, i, j+1, word, wordIndex+1, visited))
+            	return true;
         
         visited[i][j] = false;
         
         return false;
     }
     
+    public static void main(String[] args) {
+		WordSearch wordSearch = new WordSearch();
+		char[][] board = new char[][] {
+				{'A','B','C','E'},
+				{'S','F','C','S'},
+				{'A','D','E','E'}
+			};
+		System.out.println(wordSearch.exist(board, "ABCCED"));	//	true
+		System.out.println(wordSearch.exist(board, "ABCB"));	//	false
+	}
 }
 
 /*
