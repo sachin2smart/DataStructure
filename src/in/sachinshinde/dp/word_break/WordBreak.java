00fd5ac3fb83f622679af85b9002cbf6540d676c
Sachin Shinde <sachin2smart@gmail.com>
2022-09-19 07:41:32 -0400

    Word Break - DP - Leetcode

--- src/in/sachinshinde/dp/word_break/WordBreak.java
+++ src/in/sachinshinde/dp/word_break/WordBreak.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
+//	https://leetcode.com/problems/word-break/
+
 /*	
  	Given a string s and a dictionary of strings wordDict, 
  		return true if s can be segmented into a space-separated sequence of one or more dictionary words.
@@ -31,22 +33,29 @@ public class WordBreak {
 		
 		int n = s.length();
 		boolean[] dp = new boolean[n+1];
-		dp[n] = true;
 		
-		for(int i = n-1; i >= 0; i--) {
+		dp[n] = true;	//	bottom-up approach (Memoization)
+		
+		for(int i = n-1; i >= 0; i--) {	//	from the last char to first char
 			TrieNode currNode = root;
 			int j = i;
-			while(j<n) {
-				currNode = currNode.children[s.charAt(j++)-'a'];
+			
+			while(j < n) {	// from current i'th position to end of given string 
+				currNode = currNode.children[s.charAt(j++) - 'a'];
+		
 				if(currNode == null)
 					break;
-				if(currNode.isWord && dp[j]) {
+				
+				// below, the "&& dp[j]" checks all continuous words from previuos iterations
+				// 	it means - we are memoizing the previous result for current condition 
+				if(currNode.isWord && dp[j]) {	
 					dp[i] = true;
 					break;
 				}
 			}
 		}
-		return dp[0];
+		
+		return dp[0];	//	result should exists at the first element
 	}
 
 	private TrieNode buildTrie(List<String> wordDict) {
@@ -66,16 +75,30 @@ public class WordBreak {
 	public static void main(String[] args) {
 		WordBreak wordBreak = new WordBreak();
 		
-		String s = "iloveaaaaleetcode";
-		List<String> wordList = Arrays.asList("i","love","leet", "code","a");
+		String s = "love";
+		List<String> wordList = Arrays.asList("l","o","v","e");
+		//	Result : true
+		System.out.println(wordBreak.wordBreak(s, wordList));
+		System.out.println(wordBreak.wordBreak2(s, wordList));
+		System.out.println(wordBreak.wordBreak3(s, wordList));
 		
+		s = "iloveaaaaleetcode";
+		wordList = Arrays.asList("love","a","i","code","leet");
+		//	Result : true
+		System.out.println(wordBreak.wordBreak(s, wordList));
+		System.out.println(wordBreak.wordBreak2(s, wordList));
+		System.out.println(wordBreak.wordBreak3(s, wordList));
+		
+		s = "ialsolovegfg";
+		wordList = Arrays.asList("love","a","i","code","leet");
+		//	Result : false
 		System.out.println(wordBreak.wordBreak(s, wordList));
 		System.out.println(wordBreak.wordBreak2(s, wordList));
 		System.out.println(wordBreak.wordBreak3(s, wordList));
 	}
 	
 	// memoization
-    HashMap<String, Boolean> map = new HashMap<>();
+    HashMap<String, Boolean> map = new HashMap<>();	// we must not use "boolean" here, should be "Boolean"
     public boolean wordBreak2(String s, List<String> wordDict) {
         if(wordDict.contains(s)) 
         	return true;
@@ -83,11 +106,14 @@ public class WordBreak {
         if(map.containsKey(s)) 
         	return map.get(s);
         
-        for(int i=0;i<s.length();i++) {
-            String prefix = s.substring(0,i+1);
-            String suffix = s.substring(i+1);
+        for(int i = 0; i < s.length(); i++) {
+            String prefix = s.substring(0, i + 1);
+            String suffix = s.substring(i + 1);
             
-            if(wordDict.contains(prefix) && wordBreak(suffix, wordDict)) {
+            // prefix needs to be exists in wordDict
+            // suffix to be checked in recursion 
+            if(wordDict.contains(prefix) && 
+            		wordBreak2(suffix, wordDict)) {
                 map.put(s, true);
                 return true;
             }
