a25717bc5566f964d8741e7a472def72b6551f4c
Sachin Shinde <sachin2smart@gmail.com>
2022-10-02 08:38:15 -0400

    Minimum Window Substring - Arrays - Leetcode

--- src/in/sachinshinde/array/MinimumWindowSubstring.java
+++ src/in/sachinshinde/array/MinimumWindowSubstring.java
@@ -95,5 +95,52 @@ public class MinimumWindowSubstring {
 	System.out.println(w.minWindow("ADOBECODEBANC", "ABC"));//	BANC
 	System.out.println(w.minWindow("a", "a"));//	"a"
 	System.out.println(w.minWindow("a", "aa"));//	""
+	
+	System.out.println(w.minWindow2("ABAACBAB", "ABC"));	//	ACB
+	System.out.println(w.minWindow2("ADOBECODEBANC", "ABC"));//	BANC
+	System.out.println(w.minWindow2("a", "a"));//	"a"
+	System.out.println(w.minWindow2("a", "aa"));//	""
+    }
+    
+    // Confusing, so skip this
+    public String minWindow2(String s, String t) {
+        if (s.length() == 0 || t.length() == 0) 
+            return "";
+        
+        Map<Character, Integer> hm = new HashMap<Character, Integer>();
+        for (int i = 0; i < t.length(); i++)
+            hm.compute(t.charAt(i), (k, v) -> v == null ? 1 : v + 1);
+        
+        int want = t.length(), start = 0, end = -1;
+        for (int i = 0, j = 0; j < s.length(); ++j) {
+            char c = s.charAt(j);            
+            Integer count = hm.get(c);
+			
+            if (count != null && count > 0)
+                --want;
+            
+            hm.put(c, hm.getOrDefault(c, 0) - 1);
+            
+            if (want != 0) 
+				continue;
+            
+            // think s=xyabc and t=abc
+            while (i < j && hm.get(s.charAt(i)) < 0) {                
+                hm.put(s.charAt(i), hm.get(s.charAt(i)) + 1);
+                i++;
+            }
+
+            if (end == -1 || (j - i) < (end -start)) {
+                start = i;
+                end = j;
+            }
+            
+            // think s=xyabxycab and t=abc
+            want++;
+            hm.put(s.charAt(i), hm.get(s.charAt(i)) + 1);
+            i++;
+        }
+        
+        return s.substring(start, end + 1);
     }
 }
