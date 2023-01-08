80e6288f98945a89cb897b83395924f25a19b57e
Sachin Shinde <sachin2smart@gmail.com>
2023-01-08 12:26:35 -0500

    SubArraySumEqualsK - HashMap,PrefixSum - Leetcode

---
+++ src/in/sachinshinde/hashmap/SubarraySumEqualsK.java
@@ -0,0 +1,78 @@
+package in.sachinshinde.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//	https://leetcode.com/problems/subarray-sum-equals-k/
+//	Video Solution : https://youtu.be/fFVZt-6sgyo
+
+/*
+ 	Given an array of integers nums and an integer k, 
+ 		return the total number of subarrays whose sum equals to k.
+
+        A subarray is a "contiguous" non-empty sequence of elements within an array.
+        
+        
+        Example 1:
+        ---------
+        Input: nums = [1,1,1], k = 2
+        Output: 2
+        
+        Example 2:
+        ---------
+        Input: nums = [1,2,3], k = 3
+        Output: 2
+        
+        Constraints:
+        -----------
+        	1 <= nums.length <= 2 * 104
+        	-1000 <= nums[i] <= 1000
+        	-107 <= k <= 107 
+ */
+
+public class SubarraySumEqualsK {
+    
+    public int subarraySum(int[] nums, int k) { 
+	// Map : PrefixSum -> Count
+        Map<Integer, Integer> hm = new HashMap<>();
+        int sum = 0, res = 0;
+        
+        for(int num: nums) {
+            sum += num;
+            
+            if(sum == k)
+        	res++;
+            
+            if(hm.containsKey(sum - k))		// if prefix sum found, add the corresponding count into the result
+        	res += hm.get(sum -k);
+            
+            hm.put(sum, hm.getOrDefault(sum, 0) + 1);
+        }
+        
+        return res;
+    }
+    
+    public int subarraySum_2(int[] nums, int k) { 
+	// Map : PrefixSum -> Count
+        Map<Integer, Integer> hm = new HashMap<>();
+        int sum = 0, res = 0;
+        hm.put(0, 1);
+        
+        for(int num: nums) {
+            sum += num;
+            
+            if(hm.get(sum-k) != null)	//	hm value is of Integer type, if the key does not have a value - it will return a NULL 
+        	res += hm.get(sum-k);
+             
+            hm.put(sum, hm.getOrDefault(sum, 0) + 1);
+        }
+        
+        return res;
+    }
+    
+    public static void main(String[] args) {
+	SubarraySumEqualsK sum = new SubarraySumEqualsK();
+	System.out.println(sum.subarraySum(new int[] {1,-1,1,1,1,1}, 3));	// 4
+	System.out.println(sum.subarraySum_2(new int[] {1,-1,1,1,1,1}, 3));	// 4
+    }
+}
