953d2cbdab71ff08ac2653b1d54fb666f04fca07
Sachin Shinde <sachin2smart@gmail.com>
2023-12-30 09:09:57 -0500

    Decode Ways - DP(String) - Leetcode

--- src/in/sachinshinde/dp/string/DecodeWays.java
+++ src/in/sachinshinde/dp/string/DecodeWays.java
@@ -109,20 +109,20 @@ public class DecodeWays {
 	if(n == 0)
 	    return 0;
 	
-	int[] dp = new int[n+1];
-	dp[n] = 1;
+	int[] memo = new int[n+1];
+	memo[n] = 1;
 	
 	for(int i = n-1; i >= 0; i--) {
 	    if(s.charAt(i) == '0')
-		dp[i] = 0;
+		memo[i] = 0;
 	    else {
-		dp[i] += dp[i+1];
+		memo[i] += memo[i+1];
 		if(i < n-1 && (s.charAt(i) == '1' || (s.charAt(i) == '2' && s.charAt(i+1) < '7')))
-		    dp[i] += dp[i+2];
+		    memo[i] += memo[i+2];
 	    }
 	}
 	
-	return dp[0];
+	return memo[0];
     }
 //	--------------------------------------------------------------------	//
     
