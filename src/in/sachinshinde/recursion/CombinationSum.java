1bf4435fe9e13859ca39d0620fce06d2f0d4d859
Sachin Shinde <sachin2smart@gmail.com>
2024-01-12 15:31:51 -0500

    Combination Sum 1 : Recursion - Leetcode

--- src/in/sachinshinde/recursion/CombinationSum.java
+++ src/in/sachinshinde/recursion/CombinationSum.java
@@ -40,6 +40,30 @@ public class CombinationSum {
         }
     }
     
+    // Using Stirver's recursion technique, video: https://youtu.be/OyZFFqQtu98
+    
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> subsets = new ArrayList<>();
+        constructSubsets(0, candidates, target, subsets, new ArrayList<>());
+        return subsets;
+    }
+    
+    private void constructSubsets(int start, int[] candidates, int target, List<List<Integer>> subsets, List<Integer> currSumArray) {
+        if(start == candidates.length) {
+            if(target == 0) {
+                subsets.add(new ArrayList<>(currSumArray));
+            }
+            return;
+        }
+        
+        if(candidates[start] <= target) {
+            currSumArray.add(candidates[start]);
+            constructSubsets(start, candidates, target - candidates[start], subsets, currSumArray);
+            currSumArray.remove(currSumArray.size() - 1);
+        }
+        constructSubsets(start + 1, candidates, target, subsets, currSumArray);
+    }
+    
     public static void main(String args[]) {
         int[] nums = new int[] {2,3,6,7};
         List<List<Integer>> ans = getCombinationSumWithDups(nums, 7);
@@ -47,5 +71,8 @@ public class CombinationSum {
         System.out.println("The subsets with all permutations ");
         for(List<Integer> tupple: ans)
             System.out.println(" "+ tupple);
+        
+        CombinationSum combinationSum = new CombinationSum();
+        System.out.println(combinationSum.combinationSum(new int[] {2,3,6,7}, 7));	//	[[2, 2, 3], [7]]
     }
 }
