c3f9a2a98c0a4c67f8416d24f33e93d1830a37e5
Sachin Shinde <sachin2smart@gmail.com>
2024-01-16 20:44:25 -0500

    Unique BSTs - BST(Dynamic Programming) - Leetcode

--- src/in/sachinshinde/bst/UniqueBSTs.java
+++ src/in/sachinshinde/bst/UniqueBSTs.java
@@ -2,24 +2,60 @@ package in.sachinshinde.bst;
 
 import java.util.Arrays;
 
+//	https://leetcode.com/problems/unique-binary-search-trees/
+
+/*
+ 	Given an integer n, return the number of structurally unique BST's (binary search trees) 
+ 		which has exactly n nodes of unique values from 1 to n.
+
+
+        Example 1:
+        ---------
+        Input: n = 3
+        Output: 5
+        
+        Example 2:
+        ---------
+        Input: n = 1
+        Output: 1
+        
+        Constraints:
+        	1 <= n <= 19
+ */
+
+//	Video Link: https://youtu.be/Ox0TenN3Zpg
+
 public class UniqueBSTs {
 
-	static int numberOfUniqueBSTs(int numOfnodes) {
-		int dp[] = new int[numOfnodes+1];
-		Arrays.fill(dp, 0);
+    // Using Dynamic Programming 
+    /*
+    	numTrees[4] = numTrees[0] * numTrees[3]  +
+     		      numTrees[1] * numTrees[2]	 +
+     		      numTrees[2] * numTrees[1]	 +
+     		      numTrees[3] * numTrees[1]
+     */
+    
+    public int numTrees(int n) {
+        int dp[] = new int[n+1];
+	Arrays.fill(dp, 0);
 		
-		dp[0] = 1;
-		dp[1] = 1;
+	dp[0] = 1;	// with 0 nodes - we could have 1 tree 
+	dp[1] = 1;	// with 1 nodes - we could have 1 tree
 		
-		for(int i=2; i<=numOfnodes; i++) 
-			for(int j=1; j<=i; j++) 
-				dp[i] = dp[i] + (dp[i-j]*dp[j-1]);
-
-		return dp[numOfnodes];
+	for(int i=2; i<=n; i++) {
+	    for(int j=1; j<=i; j++) {
+		int numOfLeftSubTrees = i-j;
+		int numOfRightSubTrees = j-1;
+		dp[i] += dp[numOfLeftSubTrees] * dp[numOfRightSubTrees];
+	    }
 	}
 	
+	return dp[n];
+    }
+	
 	public static void main(String args[]) {
-		int k= 4;
-		System.out.println(" The number of unique BSTs for " + k + " are " + numberOfUniqueBSTs(k));
+	    UniqueBSTs bst = new UniqueBSTs();
+	    System.out.println(bst.numTrees(3));	// 5
+	    System.out.println(bst.numTrees(1));	// 1
 	}
 }
