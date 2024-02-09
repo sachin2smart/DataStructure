dfbf21299865bd94a12732dd7a10bc510f6ea776
Sachin Shinde <sachin2smart@gmail.com>
2024-02-08 19:53:33 -0500

    PriorityQueue Initialization

---
+++ src/in/sachinshinde/z_basicsjava/PriorityQueueInitialization.java
@@ -0,0 +1,70 @@
+package in.sachinshinde.z_basicsjava;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.TreeMap;
+
+public class PriorityQueueInitialization {
+
+    public static void main(String[] args) {
+//	------------------------------------------------------------------------------------------------------------------
+	
+	Queue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
+	
+//	------------------------------------------------------------------------------------------------------------------	
+	
+	Queue<Integer> pq1 = new PriorityQueue<>((a, b) -> (b - a));
+	
+//	------------------------------------------------------------------------------------------------------------------
+	
+	Queue<int[]> pq2 = new PriorityQueue<>((a,b) -> { return Integer.compare(a[0], b[0]); });
+	
+//	------------------------------------------------------------------------------------------------------------------
+	
+	Queue<int[]> pq3 = new PriorityQueue<>((a,b) -> Integer.compare(a[1], b[1]));
+	
+//	------------------------------------------------------------------------------------------------------------------
+	
+	Queue<int[]> pq4 = new PriorityQueue<>((a,b) -> a[2] - b[2]);
+	
+//	------------------------------------------------------------------------------------------------------------------
+	
+	TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>> treemap = new TreeMap<>();
+	
+//	------------------------------------------------------------------------------------------------------------------		
+	
+	Queue<Map.Entry<Integer, Integer>> q = new PriorityQueue<>((a,b)->b.getValue()-a.getValue());
+	
+//	------------------------------------------------------------------------------------------------------------------	
+	
+	Node[] lists = new Node[] {};
+	Queue<Node> qNode = new PriorityQueue<>(lists.length, (a,b) -> a.key - b.key);
+	
+//	------------------------------------------------------------------------------------------------------------------
+	
+	Queue<Map.Entry<String, Integer>> pqOfMapEntry = 
+    		new PriorityQueue<>((a,b) -> a.getValue() == b.getValue() ?
+    					b.getKey().compareTo(a.getKey()) : 
+    						a.getValue() - b.getValue());
+	
+//	------------------------------------------------------------------------------------------------------------------
+	
+	List<List<Integer>> nums = new ArrayList<>();
+	int[] next = new int[10];
+	Queue <Integer> min_queue = new PriorityQueue <Integer> (
+        	(i, j) -> nums.get(i).get(next[i]) - nums.get(j).get(next[j]));
+	
+//	------------------------------------------------------------------------------------------------------------------
+	
+	Map<Integer, Integer> map = new HashMap<>();
+	Queue<Integer> heap = new PriorityQueue<>((a, b) -> map.get(b) - map.get(a));
+	
+//	------------------------------------------------------------------------------------------------------------------
+    }
+    
+}
