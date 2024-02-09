8a5fb3bbc765fcf118e5cdf2fda1d2e3ee44acd0
Sachin Shinde <sachin2smart@gmail.com>
2024-02-08 20:26:21 -0500

    Minimum Number of Swaps to Make the String Balanced - Math - Leetcode

---
+++ src/in/sachinshinde/stack/MinimumNumberOfSwapsToMakeTheStringBalanced.java
@@ -0,0 +1,122 @@
+package in.sachinshinde.stack;
+
+//	https://leetcode.com/problems/minimum-number-of-swaps-to-make-the-string-balanced/
+
+/*
+ 	You are given a 0-indexed string s of even length n. 
+ 	The string consists of exactly n / 2 opening brackets '[' and n / 2 closing brackets ']'.
+
+        A string is called balanced if and only if:
+        
+        It is the empty string, or
+        It can be written as AB, where both A and B are balanced strings, or
+        It can be written as [C], where C is a balanced string.
+        You may swap the brackets at any two indices any number of times.
+        
+        Return the minimum number of swaps to make s balanced.
+        
+         
+        
+        Example 1:
+        ----------
+        Input: s = "][]["
+            Output: 1
+            Explanation: You can make the string balanced by swapping index 0 with index 3.
+            The resulting string is "[[]]".
+            
+        Example 2:
+        ----------
+            Input: s = "]]][[["
+            Output: 2
+            Explanation: You can do the following to make the string balanced:
+            - Swap index 0 with index 4. s = "[]][][".
+            - Swap index 1 with index 5. s = "[[][]]".
+            The resulting string is "[[][]]".
+        
+        Example 3:
+        ----------
+            Input: s = "[]"
+            Output: 0
+            Explanation: The string is already balanced.
+         
+        
+        Constraints:
+        ------------
+            n == s.length
+            2 <= n <= 106
+            n is even.
+            s[i] is either '[' or ']'.
+            The number of opening brackets '[' equals n / 2, and the number of closing brackets ']' equals n / 2.
+ */
+public class MinimumNumberOfSwapsToMakeTheStringBalanced {
+
+    public int minSwaps(String s) {
+        int close = 0, maxClose = 0;
+        
+        for(Character c: s.toCharArray()) {
+            if(c.equals('['))
+        	close--;
+            else
+        	close++;
+            
+            maxClose = Math.max(maxClose, close);
+        }
+        
+        return (maxClose + 1) / 2;
+    }
+    
+    public int minSwaps2(String s) {
+        int close = 0, maxClose = 0;
+        
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i)  == '[')
+        	close--;
+            else
+        	close++;
+            
+            maxClose = Math.max(maxClose, close);
+        }
+        
+        return (maxClose + 1) / 2;
+    }
+    
+    public int minSwaps3(String s) {
+        int close = 0, maxClose = 0;
+        
+        for(Character c: s.toCharArray()) {
+            if(c == '[')
+        	close--;
+            else
+        	close++;
+            
+            maxClose = Math.max(maxClose, close);
+        }
+        
+        return (maxClose + 1) / 2;
+    }
+    
+    public int minSwaps4(String s) {
+        int close = 0;
+        
+        for(char c: s.toCharArray()) {
+            if(c == '[')
+        	close++;
+            else if(close > 0)
+        	close--;
+        }
+        
+        return (close + 1) / 2;
+    }
+    
+    public static void main(String[] args) {
+	MinimumNumberOfSwapsToMakeTheStringBalanced str = new MinimumNumberOfSwapsToMakeTheStringBalanced();
+	
+	System.out.println(str.minSwaps("][]["));		//	1
+	System.out.println(str.minSwaps("]]][[["));		//	2
+	System.out.println(str.minSwaps("[]"));			//	0
+	
+	System.out.println(str.minSwaps4("][]["));		//	1
+	System.out.println(str.minSwaps4("]]][[["));		//	2
+	System.out.println(str.minSwaps4("[]"));			//	0
+    }
+}
