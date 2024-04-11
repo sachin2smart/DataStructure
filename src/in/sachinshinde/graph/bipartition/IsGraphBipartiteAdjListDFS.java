3fb37ce4f09d0176f0544fcb3b5b2f6c4693f000
Sachin Shinde <sachin2smart@gmail.com>
2024-04-11 19:04:12 -0400

    Graph Bipartition Check using DFS when Graph is given in Adjacency List format

--- src/in/sachinshinde/graph/bipartition/IsGraphBipartiteAdjListDFS.java
+++ src/in/sachinshinde/graph/bipartition/IsGraphBipartiteAdjListDFS.java
@@ -1,6 +1,7 @@
 package in.sachinshinde.graph.bipartition;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 //	-1	: not colored
 //	0	: blue color
@@ -9,38 +10,42 @@ import java.util.ArrayList;
 public class IsGraphBipartiteAdjListDFS {
 
 	private static boolean isBipartite(ArrayList<ArrayList<Integer>> adjList, int n) {
-		int color[] = new int[n];
-		for(int i = 0;i<n;i++)
-			color[i] = -1; 
+		int[] color = new int[n];
+		Arrays.fill(color, -1);
 		
-		for(int node=0; node<n; node++)
-			if(color[node] == -1)
-				if(!isAdjColoringValid(adjList, node, color))
-					return false; 
+		for(int node=0; node<n; node++) {
+			if (color[node] == -1) {
+				if (!validColoring(adjList, node, color)) {
+					return false;
+				}
+			}
+		}
 				
 		return true;
 	}
 	
-	private static 	boolean isAdjColoringValid(ArrayList<ArrayList<Integer>> adjList, int node, int color[]) {
+	private static 	boolean validColoring(ArrayList<ArrayList<Integer>> adjList, int node, int[] color) {
 		for(Integer adjNode: adjList.get(node)) {
 			if(color[adjNode] == -1) {
 				color[adjNode] = 1 - color[node]; 
 
-				if(!isAdjColoringValid(adjList, adjNode, color)) 
-					return false; 
+				if(!validColoring(adjList, adjNode, color)) {
+					return false;
+				}
 			}
-			else if(color[adjNode] == color[node])
-				return false; 
+			else if(color[adjNode] == color[node]) {
+				return false;
+			}
 		}
-		return true; 
+		return true;
 	}
 	
 	public static void main(String[] args) {
 		int n = 4;
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
 		
 		for(int i = 0; i < n; i++) 
-			adj.add(new ArrayList<Integer>());
+			adj.add(new ArrayList<>());
 			
 		adj.get(0).add(1);
 		adj.get(1).add(0);
@@ -63,10 +68,9 @@ public class IsGraphBipartiteAdjListDFS {
 			System.out.println("Not Bipartite"); // result
 		
 		// Test-2
-		n = 4;
-		adj = new ArrayList<ArrayList<Integer>>();
+		adj = new ArrayList<>();
 		for (int i = 0; i < n; i++) 
-			adj.add(new ArrayList<Integer>());
+			adj.add(new ArrayList<>());
 		
 		adj.get(0).add(1);
 		adj.get(1).add(0);
