286761f57178da5f8dac8f57b862507ac88bc021
Sachin Shinde <sachin2smart@gmail.com>
2024-04-16 21:38:50 -0400

    ConstructBinaryTreeFromInorderPreorderTraversal - Binary Tree - Leetcode

--- src/in/sachinshinde/binarytree/build/ConstructBinaryTreeFromInorderPreorderTraversal.java
+++ src/in/sachinshinde/binarytree/build/ConstructBinaryTreeFromInorderPreorderTraversal.java
@@ -11,48 +11,56 @@ import java.util.Map;
 public class ConstructBinaryTreeFromInorderPreorderTraversal {
 	
 	public Node buildTree(int[] inorder, int[] preorder) {
-		if (inorder == null || preorder == null || 
-				inorder.length != preorder.length)
+        if (inorder == null || preorder == null || inorder.length != preorder.length) {
             return null;
-		
-        Map<Integer, Integer> inMap = new HashMap<Integer, Integer>();
-        
-        //  Storing the i as the i'th index value from inorder list 
-        //		into the hashmap [nodeVal = index] 
-        for(int i = 0; i < inorder.length; i++)
+        }
+
+        Map<Integer, Integer> inMap = new HashMap<>();
+
+        //  Storing the i as the i'th index value from inorder list into the hashmap [nodeVal = index]
+        for (int i = 0; i < inorder.length; i++) {
             inMap.put(inorder[i], i);
-        
-        return buildTree(inorder, 0, inorder.length - 1, 
-        					preorder, 0, preorder.length - 1, 
-        					inMap);
+        }
+
+        return buildTree(0, preorder.length - 1, preorder, 0, inorder.length - 1, inMap);
     }
 
-    public Node buildTree(int[] inorder, int inStart, int inEnd,
-    						int[] preorder, int preStart, int preEnd,
-    						Map<Integer, Integer> inMap) {
-        if(inStart > inEnd || preStart > preEnd) 
-        	return null;
-
+    public Node buildTree(int preStart, int preEnd, int[] preorder, int inStart, int inEnd, Map<Integer, Integer> inMap) {
+        if(inStart > inEnd || preStart > preEnd) {
+            return null;
+        }
         // first node from the preOrder list is the root node
         Node root = new Node(preorder[preStart]);
-        
         // Get the index position from the inorder list
         int inRoot = inMap.get(root.key);
-
-        int deltaIndex = preStart + inRoot - inStart;
+        // Determine number of nodes on left side for left subtree
+        int numsLeft = (preStart - inStart) + inRoot;
         
         //  inorder  = inStart.......inRoot-1......inRoot+1.......inEnd
-        //	preorder = preStart+1....deltaIndex....deltaIndex+1...preEnd
+        //	preorder = preStart+1....numsLeft....numsLeft+1...preEnd
         
-        root.left = buildTree(inorder, inStart, inRoot - 1, 
-        						preorder, preStart + 1, deltaIndex, 
-        						inMap);
-        
-        root.right = buildTree(inorder, inRoot + 1, inEnd,
-        						preorder, deltaIndex + 1, preEnd, 
-        						inMap);
+        root.left = buildTree(preStart + 1, numsLeft, preorder, inStart, inRoot - 1, inMap);
+        root.right = buildTree(numsLeft + 1, preEnd, preorder, inRoot + 1, inEnd, inMap);
 
         return root;
     }
-	
+
+    public static void main(String[] args) {
+        ConstructBinaryTreeFromInorderPreorderTraversal tree = new ConstructBinaryTreeFromInorderPreorderTraversal();
+        int[] preorder = new int[] {10,20,40,50,30,60};
+        int[] inorder = new int[] {40,20,50,10,60,30};
+
+        Node root = tree.buildTree(inorder, preorder);
+        tree.preOrder(root); // 10 20 40 50 30 60
+    }
+
+    private void preOrder(Node root) {
+        if( root == null) {
+            return;
+        }
+
+        System.out.print(" "+ root.key);
+        preOrder(root.left);
+        preOrder(root.right);
+    }
 }
