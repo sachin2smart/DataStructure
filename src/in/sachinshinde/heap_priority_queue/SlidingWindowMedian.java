6919f3d9ad488c7eacc5c34a3427cee9e30a9cad
Sachin Shinde <sachin2smart@gmail.com>
2024-06-26 13:20:58 -0400

    Sliding Window Median - Heap(PQ) - Leetcode

--- src/in/sachinshinde/heap_priority_queue/SlidingWindowMedian.java
+++ src/in/sachinshinde/heap_priority_queue/SlidingWindowMedian.java
@@ -1,6 +1,6 @@
 package in.sachinshinde.heap_priority_queue;
 
-import java.util.TreeSet;
+import java.util.*;
 
 //  https://leetcode.com/problems/sliding-window-median/
 
@@ -77,8 +77,104 @@ public class SlidingWindowMedian {
 
     public static void main(String[] args) {
         SlidingWindowMedian slidingWindowMedian = new SlidingWindowMedian();
-        System.out.println(slidingWindowMedian.medianSlidingWindow(new int[]{1,3,-1,-3,5,3,6,7}, 3));   //  [1.00000,-1.00000,-1.00000,3.00000,5.00000,6.00000]
-        System.out.println(slidingWindowMedian.medianSlidingWindow(new int[]{1,2,3,4,2,3,1,4,2}, 3));   //  [2.00000,3.00000,3.00000,3.00000,2.00000,3.00000,2.00000]
+        System.out.println(Arrays.toString(slidingWindowMedian.medianSlidingWindow(new int[]{1,3,-1,-3,5,3,6,7}, 3)));   //  [1.00000,-1.00000,-1.00000,3.00000,5.00000,6.00000]
+        System.out.println(Arrays.toString(slidingWindowMedian.medianSlidingWindow(new int[]{1,2,3,4,2,3,1,4,2}, 3)));   //  [2.00000,3.00000,3.00000,3.00000,2.00000,3.00000,2.00000]
 
+        System.out.println(Arrays.toString(slidingWindowMedian.medianSlidingWindow2(new int[]{1,3,-1,-3,5,3,6,7}, 3)));   //  [1.00000,-1.00000,-1.00000,3.00000,5.00000,6.00000]
+        System.out.println(Arrays.toString(slidingWindowMedian.medianSlidingWindow2(new int[]{1,2,3,4,2,3,1,4,2}, 3)));   //  [2.00000,3.00000,3.00000,3.00000,2.00000,3.00000,2.00000]
+
+        System.out.println(Arrays.toString(slidingWindowMedian.medianSlidingWindow3(new int[]{1,3,-1,-3,5,3,6,7}, 3)));   //  [1.00000,-1.00000,-1.00000,3.00000,5.00000,6.00000]
+        System.out.println(Arrays.toString(slidingWindowMedian.medianSlidingWindow3(new int[]{1,2,3,4,2,3,1,4,2}, 3)));   //  [2.00000,3.00000,3.00000,3.00000,2.00000,3.00000,2.00000]
+    }
+
+    //  Second Approach
+    public double[] medianSlidingWindow2(int[] nums, int k) {
+        double[] medianArray = new double[nums.length - k + 1];
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0, j = 0; i < nums.length; i++) {
+            addElement (list, nums[i]);
+            if (i >= k - 1) {
+                medianArray[j++] = findMedian(list, k);
+                removeElement(list, nums[i - k + 1]);
+            }
+        }
+
+        return medianArray;
+    }
+
+    public void addElement (List<Integer> list, int num) {
+        int index = Collections.binarySearch(list, num);
+        index = index < 0 ? Math.abs(index) - 1 : index;
+        list.add(index, num);
+    }
+
+    public void removeElement(List<Integer> list, int num) {
+        int index = Collections.binarySearch(list, num);
+        list.remove(index);
+    }
+
+    public double findMedian(List<Integer> list, int k) {
+        return k % 2 == 0 ? ((double) list.get(k / 2 - 1) + list.get(k / 2)) / 2 : list.get(k / 2);
+    }
+
+    //  Third Approach
+    public double[] medianSlidingWindow3(int[] nums, int k) {
+        if (nums == null || k <= 0) {
+            throw new IllegalArgumentException("Input is invalid");
+        }
+
+        int len = nums.length;
+        double[] result = new double[len - k + 1];
+        if (k == 1) {
+            for (int i = 0; i < len; i++) {
+                result[i] = (double) nums[i];
+            }
+            return result;
+        }
+
+        PriorityQueue<Integer> smallNums = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> largeNums = new PriorityQueue<>();
+
+        for (int i = 0; i < len; i++) {
+            if (i >= k) {
+                removeElement(smallNums, largeNums, nums[i - k]);
+            }
+            addElement(smallNums, largeNums, nums[i]);
+            if (i >= k - 1) {
+                result[i - (k - 1)] = getMedian(smallNums, largeNums);
+            }
+        }
+
+        return result;
+    }
+
+    private void addElement(PriorityQueue<Integer> smallNums, PriorityQueue<Integer> largeNums, int n) {
+        smallNums.offer(n);
+        largeNums.offer(smallNums.poll());
+        if (smallNums.size() < largeNums.size()) {
+            smallNums.offer(largeNums.poll());
+        }
+    }
+
+    private void removeElement(PriorityQueue<Integer> smallNums, PriorityQueue<Integer> largeNums, int n) {
+        if (n >= largeNums.peek()) {
+            largeNums.remove(n);
+            if (smallNums.size() == largeNums.size() + 2) {
+                largeNums.offer(smallNums.poll());
+            }
+        } else {
+            smallNums.remove(n);
+            if (smallNums.size() < largeNums.size()) {
+                smallNums.offer(largeNums.poll());
+            }
+        }
+    }
+
+    private double getMedian(PriorityQueue<Integer> smallNums, PriorityQueue<Integer> largeNums) {
+        if (smallNums.size() == largeNums.size()) {
+            return ((double) smallNums.peek() + largeNums.peek()) / 2.0;
+        }
+        return smallNums.peek();
     }
 }
