e12d403c38e996b5df2205f44bf98d4a9e8e1238
Sachin Shinde <sachin2smart@gmail.com>
2025-03-26 20:05:24 -0400

    Minimum in Rotated Sorted Sorted Array

--- src/in/sachinshinde/array/MinimumInRotatedSortedArray.java
+++ src/in/sachinshinde/array/MinimumInRotatedSortedArray.java
@@ -66,5 +66,26 @@ public class MinimumInRotatedSortedArray {
         System.out.println(array.findMin(new int[] {3,4,5,1,2})); 	// 1
         System.out.println(array.findMin(new int[] {4,5,6,7,0,1,2})); 	// 0
         System.out.println(array.findMin(new int[] {11,13,15,17})); 	// 11
+
+        System.out.println(array.findMax(new int[] {3,4,5,1,2})); 	// 5
+        System.out.println(array.findMax(new int[] {4,5,6,7,0,1,2})); 	// 7
+        System.out.println(array.findMax(new int[] {11,13,15,17})); 	// 17
+    }
+
+    public int findMax(int[] nums) {
+        int l = 0;
+        int r = nums.length - 1;
+
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] > nums[r]) {
+                r = mid;
+            }
+            else {
+                l = mid + 1;
+            }
+        }
+
+        return nums[r];
     }
 }
